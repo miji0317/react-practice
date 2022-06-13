7e9ed84b7a0f443c443308b28624db33ed403f87
miji <mijiunknown@gmail.com>
2022-06-13 13:24:24 +0900

    styled-components 적용

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import {useState} from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import styled from 'styled-components';
 
-function Header(props) {
-  console.log(props);
-  return <header><h1><a href="/" onClick={(evt) => {
-    console.log('evt', evt);
-    evt.preventDefault();
+// Header 컴포넌트
+function Header(props){
+  return <header className={props.className}><h1><a href="/" onClick={(evt)=>{    
+    evt.preventDefault(); 
     props.onSelect();
-  }}>Web</a></h1></header>
+  }}>WWW</a></h1></header>
 }
 
+// styled-components 적용
+const HeaderStyled = styled(Header)`
+  border-bottom : 1px solid gray;
+  padding: 20px;
+  fontSize: 20px;
+`;
+
+// Nav 컴포넌트
 function Nav(props) {
   const list = props.data.map((e) => {
     return <li key={e.id}><a href={'/read/'+e.id} onClick={(evt) => {
@@ -27,14 +35,28 @@ function Nav(props) {
   return <nav><ol>{list}</ol></nav>
 }
 
+// Article 컴포넌트
 function Article(props) {
   console.log('props', props.title)
   return <article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
-};
+}
 
+// Create 컴포넌트
+function Create() {
+  return <article>
+    <h2>Create</h2>
+    <form>
+      <p><input type='text' placeholder='title'></input></p>
+      <p><textarea placeholder='body'></textarea></p>
+      <p><input type='submit' value='Create'></input></p>
+    </form>
+  </article>
+}
+
+// App
 function App() {
   // state를 읽을 때는 mode, 바꿀 때는 setMode, 초기 상태는 useState
   const [mode, setMode] = useState('WELCOME');
@@ -58,13 +80,15 @@ function App() {
     }
   })[0];
   content = <Article title={topic.title} body={topic.body}></Article>
+  } else if (mode === 'CREATE') {
+    content = <Create></Create>;
   }
 
   return (
     <div>
-      <Header onSelect={() => {
+      <HeaderStyled onSelect={() => {
        setMode('WELCOME');
-      }}></Header>
+      }}></HeaderStyled>
       <Nav data={topics} onSelect={(id) => {
         setMode('READ');
         setId(id);
@@ -72,7 +96,7 @@ function App() {
       {content}
       <ButtonGroup variant="outlined" aria-label="outlined button group" size="small" color="secondary">
         <Button variant='outlined' onClick={() => {
-          alert('create!');
+          setMode('CREATE');
         }}>Create</Button>
         <Button variant='outlined'>Update</Button>
         <Button variant='outlined'>Delete</Button>
