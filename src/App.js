384867283232c540926f94d5abb9cf86d57aabf7
miji <mijiunknown@gmail.com>
2022-06-15 14:50:02 +0900

    ajax

--- src/App.js
+++ src/App.js
@@ -1,105 +1,101 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import styled from 'styled-components';
-import { Link, Routes, Route, useParams, useNavigate } from 'react-router-dom';
+import { Link, Routes, Route, useParams, useNavigate } from "react-router-dom";
 
-// Header 컴포넌트
 function Header(props){
   return <header className={props.className}><h1><Link to="/" onClick={(evt)=>{    
     props.onSelect();
   }}>WWW</Link></h1></header>
 }
 
-// styled-components 적용
 const HeaderStyled = styled(Header)`
-  border-bottom : 1px solid gray;
-  padding: 20px;
-  fontSize: 20px;
+    border-bottom:1px solid gray;
+    color:red;
 `;
 
-// Nav 컴포넌트
-function Nav(props) {
-  const list = props.data.map((e) => {
-    return <li key={e.id}><Link to={'/read/'+e.id} onClick={(evt) => {
-      props.onSelect(e.id);  
-    }}>{e.title}</Link></li>
-  });
-  // const list = [
-  //   <li><a href="/read/1">html</a></li>,
-  //   <li><a href="/read/2">css</a></li>
-  // ]
-  return <nav><ol>{list}</ol></nav>
-}
-
-// Article 컴포넌트
-function Article(props) {
-  console.log('props', props.title)
+function Article(props){
   return <article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
 }
 
-// Create 컴포넌트
-function Create(props) {
-  return <article>
-    <h2>Create</h2>
-    <form onSubmit={(evt) => {
-      evt.preventDefault();
-      // evt.target는 form 태그 자신을 가리킴
-      const title = evt.target.title.value;
-      const body = evt.target.body.value;
-      // onCreate는 props니까 props로 호출
-      props.onCreate(title, body);
-    }}>
-      <p><input type='text' name='title' placeholder='title'></input></p>
-      <p><textarea name='body' placeholder='body'></textarea></p>
-      <p><input type='submit' value='Create'></input></p>
-    </form>
-  </article>
+function Nav(props){
+  const liTags = props.data.map((e)=>{
+    return <li key={e.id}><Link to={'/read/'+e.id} onClick={(evt)=>{
+      props.onSelect(e.id);
+    }}>{e.title}</Link></li>
+  })
+  return <nav><ol>{liTags}</ol></nav>
 }
 
-function Read(props) {
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(evt)=>{
+      evt.preventDefault();
+      const title = evt.target.title.value;
+      const body = evt.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"></input></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>;
+}
+
+function Read(props){
   const params = useParams();
   const id = Number(params.topic_id);
-  const topic = props.topics.filter(e=>{
-    if(e.id === id) {
-      return true;
-    } else {
-      return false;
-    }
-  })[0];
+  const [topic, setTopic] = useState({title:null, body:null});
+  useEffect(()=>{    
+    (async ()=>{
+      const resp = await fetch('http://localhost:3333/topics/'+id);
+      const data = await resp.json();
+      setTopic(data);
+    })();
+  },[id]);  
   return <Article title={topic.title} body={topic.body}></Article>
 }
 
-function Control(props) {
+function Control(props){
   const params = useParams();
   const id = Number(params.topic_id);
   let contextUI = null;
-  if (id) {
+  if(id){
     contextUI = <>
       <Button variant="outlined">Update</Button>
-      <Button variant="outlined" onClick={() => {props.onDelete(id);}}>Delete</Button>
+      <Button variant="outlined" onClick={()=>{props.onDelete(id);}}>Delete</Button>
     </>
-  }
+  } 
   return <>
     <Button component={Link} to="/create" variant="outlined">Create</Button>        
     {contextUI}
   </>
 }
 
-// App
 function App() {
-  const [mode,setMode] = useState('WELCOME');  // todo 삭제
-  const [id, setId] = useState(null);  // todo 삭제
+  const [mode,setMode] = useState('WELCOME'); // todo 삭제 예정
+  const [id, setId] = useState(null); // todo 삭제 예정
   const [nextId, setNextId] = useState(3);
   const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
   ]);
+
+  const refreshTopics = async ()=>{
+    const resp = await fetch('http://localhost:3333/topics');
+    const data = await resp.json();
+    setTopics(data);
+  };
+  useEffect(()=>{    
+    refreshTopics();
+  },[]);
 
   const navigate = useNavigate();
 
@@ -109,32 +105,30 @@ function App() {
       <Nav data={topics}  onSelect={navHandler()}></Nav>
       <Routes>
         <Route path="/" element={<Article title="Welcome" body="Hello, WEB!"></Article>}></Route>
-        <Route path="/create" element={<Create onCreate={onCreateHandler()}></Create>}></Route>
+        <Route path="/create" element={<Create onCreate={onCreateHandler}></Create>}></Route>
         <Route path="/read/:topic_id" element={<Read topics={topics}></Read>}></Route>
       </Routes>
-
       <Routes>
-        {['/', '/read/:topic_id', '/update/:topic_id'].map(path => {
-          return <Route key={path} path={path} element={<Control onDelete={(id) => {
+        {['/','/read/:topic_id', '/update/:topic_id'].map(path=>{
+          return <Route key={path} path={path} element={<Control onDelete={(id)=>{
             deleteHandler(id);
           }}></Control>}></Route>
         })}
       </Routes>
     </div>
   );
-
-  // return 내부에 있던 함수들을 분리(Refactor)
-
-  function onCreateHandler() {
-    return (title, body) => {
-      const newTopic = { id: nextId, title, body };
-      const newTopics = [...topics];
-      newTopics.push(newTopic);
-      setTopics(newTopics);
-      setId(nextId);
-      setMode('READ');
-      setNextId(nextId + 1);
-    };
+  
+  async function onCreateHandler(title, body) {
+    const resp = await fetch('http://localhost:3333/topics', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({title, body})
+    })
+    const data = await resp.json();
+    navigate(`/read/${data.id}`);
+    refreshTopics();
   }
 
   function navHandler() {
@@ -151,9 +145,9 @@ function App() {
       } else {
         return true;
       }
-    });
+    });    
     setTopics(newTopics);
-    navigate('/');  // 삭제 되면 루트로 이동
+    navigate('/');
   }
 
   function createHandler() {
