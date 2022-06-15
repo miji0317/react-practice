4b34a5424aa854139dad7746cb1e536fa39d36bd
miji <mijiunknown@gmail.com>
2022-06-15 11:51:26 +0900

    useParams

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {useState} from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route, useParams } from 'react-router-dom';
 
 // Header 컴포넌트
 function Header(props){
@@ -64,70 +64,90 @@ function Create(props) {
 
 // App
 function App() {
-  // state를 읽을 때는 mode, 바꿀 때는 setMode, 초기 상태는 useState
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(3);  // newTopic의 id
-
-  // topics를 state로 변경
+  const [mode,setMode] = useState('WELCOME');  // todo 삭제
+  const [id, setId] = useState(null);  // todo 삭제
+  const [nextId, setNextId] = useState(3);
   const [topics, setTopics] = useState([
-    {id: 1, title: 'html', body: 'html is ...'},
-    {id: 2, title: 'css', body: 'css is ...'},
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
   ]);
 
-  let content = null;
-  if (mode === 'WELCOME') {
-    content = <Article title="Welcome" body="Hello, Web!"></Article>
-  } else if (mode === 'READ') {
-    const topic = topics.filter(e => {
-    if (e.id === id) {
-      return true;
-    } else {
-      return false;
-    }
-  })[0];
-  content = <Article title={topic.title} body={topic.body}></Article>
-  } else if (mode === 'CREATE') {
-    content = <Create onCreate={(title, body) => {
+  return (
+    <div>
+      <HeaderStyled onSelect={headerHandler()}></HeaderStyled>
+      <Nav data={topics}  onSelect={navHandler()}></Nav>
+      <Routes>
+        <Route path="/" element={<Article title="Welcome" body="Hello, WEB!"></Article>}></Route>
+        <Route path="/create" element={<Create onCreate={onCreateHandler()}></Create>}></Route>
+        <Route path="/read/:topic_id" element={<Read topics={topics}></Read>}></Route>
+      </Routes>
+      <ButtonGroup>
+        <Button component={Link} to="/create" variant="outlined" onClick={createHandler()}>Create</Button>        
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined" onClick={deleteHandler()}>Delete</Button>
+    </div>
+  );
+
+  // return 내부에 있던 함수들을 분리(Refactor)
+  
+  function Read(props) {
+    const params = useParams();
+    const id = Number(params.topic_id);
+    const topic = props.topics.filter(e=>{
+      if(e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    return <Article title={topic.title} body={topic.body}></Article>
+  }
+
+  function onCreateHandler() {
+    return (title, body) => {
+      const newTopic = { id: nextId, title, body };
       const newTopics = [...topics];
-      const newTopic = {id: nextId, title, body};
       newTopics.push(newTopic);
       setTopics(newTopics);
       setId(nextId);
       setMode('READ');
-      setNextId(nextId+1);
-    }}></Create>;
+      setNextId(nextId + 1);
+    };
   }
 
-  return (
-    <div>
-      <HeaderStyled onSelect={() => {
-       setMode('WELCOME');
-      }}></HeaderStyled>
-      <Nav data={topics} onSelect={(id) => {
-        setMode('READ');
-        setId(id);
-      }}></Nav>
-      {content}
-      <ButtonGroup variant="outlined" aria-label="outlined button group" size="small" color="secondary">
-        <Button component={Link} to="/create" variant='outlined' onClick={() => {
-          setMode('CREATE');
-        }}>Create</Button>
-        <Button variant='outlined'>Update</Button>
-        <Button variant='outlined' onClick={() => {
-          const newTopics = topics.filter((e) => {
-            if (e.id === id) {
-              return false;
-            } else {
-              return true;
-            }
-          });
-          setTopics(newTopics);
-          setMode('WELCOME')
-        }}>Delete</Button>
-      </ButtonGroup>
-    </div>
-  );
+  function navHandler() {
+    return (id) => {
+      setMode('READ');
+      setId(id);
+    };
+  }
+
+  function deleteHandler() {
+    return () => {
+      const newTopics = topics.filter((e) => {
+        if (e.id === id) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      setMode('WELCOME');
+      setTopics(newTopics);
+    };
+  }
+
+  function createHandler() {
+    return () => {
+      setMode('CREATE');
+    };
+  }
+
+  function headerHandler() {
+    return () => {
+      setMode('WELCOME');
+    };
+  }
 }
 
 export default App;
